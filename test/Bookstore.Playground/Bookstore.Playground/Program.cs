eb933f66a8559928a2d62ba8de71b0b8a44a3a24
Mia Tomaić <mtomaic@omega-software.hr>
2019-10-02 16:15:59 +0200

    vježbica

--- test/Bookstore.Playground/Bookstore.Playground/Program.cs
+++ test/Bookstore.Playground/Bookstore.Playground/Program.cs
@@ -18,10 +18,22 @@ namespace Bookstore.Playground
             var rhetosServerPath = @"..\..\..\..\..\dist\BookStoreRhetosServer\RhetosServer.2.12.0";
             Directory.SetCurrentDirectory(rhetosServerPath);
             // Set commitChanges parameter to COMMIT or ROLLBACK the data changes.
-            using (var container = new RhetosTestContainer(commitChanges: false))
+            using (var container = new RhetosTestContainer(commitChanges: true))
             {
                 var context = container.Resolve<Common.ExecutionContext>();
                 var repository = context.Repository;
+
+                var allBooks = repository.Bookstore.Book.Load(t => t.NumberOfPages > 200).Dump();
+                var someBooks = repository.Bookstore.Book.Query().ToSimple().Dump();
+
+                var book = repository.Bookstore.Book.Query(new[] { new Guid("8FCD6DAA-27F1-4ABB-9556-5E0C74BA00F0") }).ToSimple();
+
+                var filter = new FilterCriteria("Title", "StartsWith", "osi");
+                var filteredBook = repository.Bookstore.Book.Query(filter);
+
+                var booksWithComments = repository.Bookstore.Comment.Query().Where(c => c.BookID != null).ToString();
+
+                repository.Bookstore.Book.Insert(new Bookstore.Book { Title = "new book", NumberOfPages = 201, Code = "A+" });
 
                 // Query data from the `Common.Claim` table:
 
