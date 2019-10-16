ac01ce08fe2e8761cb9f6d9234a4aacc0d877655
Mia Tomaić <mtomaic@omega-software.hr>
2019-10-16 15:30:51 +0200

    added unit tests

---
+++ test/BookstoreTest/BookstoreTests.cs
@@ -0,0 +1,42 @@
+﻿using Bookstore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhetos.Configuration.Autofac;
+using Rhetos.Dom.DefaultConcepts;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace BookstoreTest
+{
+    [TestClass]
+    public class BookstoreTests
+    {
+        [TestMethod]
+        public void BookInfoNoComments()
+        {
+            var rhetosServer = @"C:\Users\mtomaic\source\repos\BookStore\dist\BookStoreRhetosServer\RhetosServer.2.12.0";
+            Directory.SetCurrentDirectory(rhetosServer);
+
+            using (var rhetos = new RhetosTestContainer())
+            {
+                var repository = rhetos.Resolve<Common.DomRepository>();
+
+                var book = new Book { Title = "testna knjiga" };
+
+                repository.Bookstore.Book.Insert(book);               
+                Assert.AreEqual(0, numberOfComments(repository, book.ID));
+
+                var comment1 = new Comment { BookID = book.ID, Text = "comm" };
+                repository.Bookstore.Comment.Insert(comment1);
+                Assert.AreEqual(1, numberOfComments(repository, book.ID));
+            }
+        }
+
+        private int? numberOfComments(Common.DomRepository repository, Guid bookId)
+        {
+            return repository.Bookstore.BookInfo
+                   .Query(bi => bi.ID == bookId)
+                   .Select(bi => bi.NumberOfComments).Single();
+        }
+    }
+}
---
+++ test/BookstoreTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BookstoreTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Omega software d.o.o.")]
+[assembly: AssemblyProduct("BookstoreTest")]
+[assembly: AssemblyCopyright("Copyright © Omega software d.o.o. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c1ca44ca-d790-4a55-9788-4e8a2015a6ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
